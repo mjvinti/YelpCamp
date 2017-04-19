b6fbdf6e8490ee2f29e9f09a583beafa113e50fb
Mario Vinti <mjvinti@gmail.com>
2017-04-19 19:58:37 +0000

    initial deploy commit

---
+++ seeds.js
@@ -0,0 +1,60 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://farm3.staticflickr.com/2580/3942698066_9157ac5123.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://farm3.staticflickr.com/2311/2123340163_af7cba3be7.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+];
+
+function seedDB(){
+    // Remove all campgrounds
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        // Add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log("added a campground");
+                    // create a comment
+                    Comment.create(
+                        {
+                            text: "This place is great, but I wish they had internet",
+                            author: "Homer"
+                        }, function(err, comment){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                campground.comments.push(comment);    
+                                campground.save();
+                                console.log("Created new comment");
+                            }
+                        });
+                }
+            });    
+        });
+    });
+    
+    
+    // Add a few comments
+}
+
+module.exports = seedDB;
